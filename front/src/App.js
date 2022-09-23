b56733a71d67951ea7a5fbac56bb0375687914b6
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-09-23 17:15:14 +0200

    Front connects to API

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [testData, setTestData] = useState(undefined);
+
+  const testAPI = () => {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => setTestData({ apiResponse: res }));
+  };
+
+  useEffect(() => {
+    testAPI()
+  }, [])
+
+  useEffect(() => {
+    console.log(testData);
+  }, [testData]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {testData && <p>{testData.apiResponse}</p>}
         <a
           className="App-link"
           href="https://reactjs.org"
