ee9e1f7673e3500d23655e56c74f42121bf06557
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-09-28 08:12:34 +0200

    Remade the Fitness Table (renamed reps column to weight)

--- api/sql.js
+++ api/sql.js
@@ -2,13 +2,14 @@ const sqlite3 = require("sqlite3");
 
 const db = new sqlite3.Database("./db.sqlite");
 
-// As an example, the function ran to create the table Todo (must be run once through Node)
+// As an example, the function ran to delete and create the table Fitness (must be run once through Node)
 // db.serialize(function () {
-//   db.run("CREATE TABLE IF NOT EXISTS Todo (id INTEGER PRIMARY KEY, name TEXT)");
-//   db.run("INSERT INTO Todo (name) VALUES ('Create database')");
-//   db.run("INSERT INTO Todo (name) VALUES ('Connect database')");
-//   db.run("INSERT INTO Todo (name) VALUES ('Sleep')");
-//   db.each("SELECT id, name FROM Todo", function (err, row) {
+//   db.run("DROP TABLE Fitness");
+//   db.run("CREATE TABLE IF NOT EXISTS Fitness (id INTEGER PRIMARY KEY, muscle TEXT, localization TEXT, weight FLOAT)");
+//   db.run("INSERT INTO Fitness (muscle, localization, weight) VALUES ('Biceps (dumbbell)', 'upper', 10)");
+//   db.run("INSERT INTO Fitness (muscle, localization, weight) VALUES ('Triceps (machine)', 'upper', 10)");
+//   db.run("INSERT INTO Fitness (muscle, localization, weight) VALUES ('Leg-press', 'lower', 95)");
+//   db.each("SELECT id, name FROM Fitness", function (err, row) {
 //     console.log(row.id + ": " + row.name);
 //   });
 // });
