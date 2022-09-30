ea60202ec550f09bf498cb3d645b4d50e9533123
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-09-30 23:03:30 +0200

    Added github contribution in homepage

--- front/src/views/homepage/Home.jsx
+++ front/src/views/homepage/Home.jsx
@@ -1,39 +1,20 @@
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import Todos from "../../components/todos/Todos";
+import GithubContribution from "../../components/github contributions/GithubContribution";
 import "./Home.css";
 
-const axios = require("axios").default;
-
-export default function Home() {
-  const [testData, setTestData] = useState(undefined);
-
-  const testAPI = () => {
-    axios
-      .get("http://localhost:9000/testAPI")
-      .then((res) => setTestData(res.data))
-      .catch((err) => console.log(err));
-  };
-
-  useEffect(() => {
-    testAPI();
-  }, []);
-
-  useEffect(() => {
-    console.log(testData);
-  }, [testData]);
-
+export default function Home({ githubData }) {
   return (
     <div className="Home">
       <header className="Home-header">
-        {testData && <p>{testData}</p>}
+        <GithubContribution githubData={githubData} />
+        <Todos />
         <div>
           <Link to="/sports">
             <button>SPORT</button>
           </Link>
         </div>
-        <Todos />
       </header>
     </div>
   );
