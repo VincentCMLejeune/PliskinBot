4d5a16f1247560bd0ec648bb53463c5085fcb615
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-10-08 10:19:05 +0200

    Created table Calendar

--- api/calendar/sqlCalendar.js
+++ api/calendar/sqlCalendar.js
@@ -1,7 +1,21 @@
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("../db.sqlite");
+
 const iterateYear = require("./calendar");
 
-const year = 2020;
+const year = 2022;
 const callback = (day) => {
   console.log(day);
+  // db.run(`INSERT INTO Calendar (date, day, dayOfWeek, month, year) VALUES (${day.date}, ${day.day}, ${day.dayOfWeek} ${day.month}, ${day.year})`);
 };
-iterateYear(year, callback);
+
+db.serialize(function () {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS Calendar (id INTEGER PRIMARY KEY, date TEXT, day INTEGER, dayOfWeek INTEGER, month INTEGER, year INTEGER, occupation TEXT DEFAULT 'free')"
+  );
+  iterateYear(year, callback);
+});
+
+// db.each("SELECT * FROM Calendar", function (err, row) {
+//   console.log(row);
+// });
