e1f67b089cf9e1af4708d43f45823b8ec96188b2
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-10-08 11:12:12 +0200

    Root planning route send all data from Calendar Table

--- api/routes/planning.js
+++ api/routes/planning.js
@@ -1,8 +1,17 @@
-var express = require("express");
-var planningRouter = express.Router();
+const express = require("express");
+const planningRouter = express.Router();
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("./db.sqlite");
 
 planningRouter.get("/", function (req, res, next) {
-  res.status(200).send("Hello planning router");
+  db.all("SELECT * FROM Calendar", (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.send({ planning: rows });
+    }
+  });
 });
 
 module.exports = planningRouter;
