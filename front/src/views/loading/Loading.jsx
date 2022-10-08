227e69255162d49b53c0182035c97e1c97df7f6c
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-10-08 13:37:30 +0200

    Loading component cleanup

--- front/src/views/loading/Loading.jsx
+++ front/src/views/loading/Loading.jsx
@@ -1,17 +1,6 @@
-import { useEffect } from "react";
-
 import "./Loading.css";
 
 export default function Loading({ dataLoaded }) {
-  useEffect(() => {
-    console.log(dataLoaded);
-    // const obj = { foo: "bar", baz: 42 };
-    // console.log(Object.entries(obj));
-    // if (dataLoaded) {
-    console.log(Object.values(dataLoaded));
-    // }
-  }, [dataLoaded]);
-
   return (
     <div className="Loading">
       <h1>Loading...</h1>
