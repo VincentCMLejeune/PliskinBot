99a09622a2a115f566330402161eff7d20f827e1
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-10-08 14:35:57 +0200

    Displays months in cubes of color depending of activity (only free available atm)

--- front/src/components/planning/Planning.jsx
+++ front/src/components/planning/Planning.jsx
@@ -4,6 +4,9 @@ import "./Planning.css";
 
 export default function Planning({ calendarData }) {
   const [today, setToday] = useState(null);
+  const [colorMap, setColorMap] = useState({
+    free: "green",
+  });
   const days = [
     "Sunday",
     "Monday",
@@ -40,6 +43,18 @@ export default function Planning({ calendarData }) {
     <>
       {today && (
         <div>
+          <div>
+            <div>{months[today.getMonth()]}</div>
+            <div className="Planning-calendar-cubes">
+              {calendarData.planning.map((day, idx) => (
+                <div
+                  key={idx}
+                  className="Planning-calendar-cube"
+                  style={{ backgroundColor: colorMap[day.occupation] }}
+                ></div>
+              ))}
+            </div>
+          </div>
           <h1>Today is {days[today.getDay()]}</h1>
           {today.getDay() === 6 || today.getDay() === 0 ? (
             <div>Yay, no work</div>
