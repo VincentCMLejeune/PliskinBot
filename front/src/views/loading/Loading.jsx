064f6445fab64aaa99b178db15304ccf1423007d
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-10-08 13:36:11 +0200

    Loading screen displays items loadings and error messages if there is any

--- front/src/views/loading/Loading.jsx
+++ front/src/views/loading/Loading.jsx
@@ -1,9 +1,32 @@
+import { useEffect } from "react";
+
 import "./Loading.css";
 
-export default function Loading({ githubData }) {
+export default function Loading({ dataLoaded }) {
+  useEffect(() => {
+    console.log(dataLoaded);
+    // const obj = { foo: "bar", baz: 42 };
+    // console.log(Object.entries(obj));
+    // if (dataLoaded) {
+    console.log(Object.values(dataLoaded));
+    // }
+  }, [dataLoaded]);
+
   return (
     <div className="Loading">
       <h1>Loading...</h1>
+      <div className="Loading-items">
+        <ul>
+          {Object.values(dataLoaded).map((data, index) => (
+            <div key={index}>
+              <li className={data.isLoaded ? "Loaded-item" : ""}>
+                {data.name}
+              </li>
+              {data.error && <div className="Loaded-error">{data.error}</div>}
+            </div>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
