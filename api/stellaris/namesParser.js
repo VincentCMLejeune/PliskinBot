70b870475745b826506ae7c9967da74081deb2b8
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-10-12 21:16:15 +0200

    Names parser checks locales and updates Stellaris table with names and desc

--- api/stellaris/namesParser.js
+++ api/stellaris/namesParser.js
@@ -1,13 +1,47 @@
 const path = require("path");
 const fs = require("fs");
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("../db.sqlite");
 
+// TODO : find the source files for all techs (some are missing)
 const parseNames = () => {
   const names = fs
     .readFileSync("../assets/stellaris/locales/technology_l_english.yml")
     .toString()
     .trim()
     .split("\n");
-  console.log(names);
+  const parsedNames = {};
+  let curName = {};
+  let nameKey = "";
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i].trim();
+    if (name.includes("_effect:")) {
+      continue;
+    } else if (name.includes("_desc")) {
+      const desc = name.split('"')[1];
+      if (desc !== "" && curName.title) {
+        curName.desc = desc;
+        parsedNames[nameKey] = curName;
+        db.run(
+          "UPDATE Stellaris SET desc = ?, name = ? WHERE id = ?",
+          [curName.desc, curName.title, nameKey],
+          function (err) {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+        curName = {};
+        nameKey = "";
+      }
+    } else if (name.substring(0, 5) === "tech_") {
+      nameKey = name.split(":")[0];
+      const title = name.split('"')[1];
+      if (title !== "Hidden") {
+        curName.title = title;
+      }
+    }
+  }
 };
 
 parseNames();
